9f65f353170d02a18154fca6a19ee4b93ea12109
doyoon <doyoun0527@naver.com>
2023-12-05 20:01:38 +0900

    init commit

---
+++ src/main/java/com/raincloud/sunlightmarket/SunlightMarketApplication.java
@@ -0,0 +1,13 @@
+package com.raincloud.sunlightmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunlightMarketApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SunlightMarketApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/raincloud/sunlightmarket/SunlightMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raincloud.sunlightmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunlightMarketApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
