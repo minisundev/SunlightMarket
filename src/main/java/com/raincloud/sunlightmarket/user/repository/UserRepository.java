8cbbce7591253984dd61f6f0602d3323a31dcdb7
minhyeongKang <kang7937@gmail.com>
2023-12-06 12:01:28 +0900

    Feat : UserEntity 및 Repository

---
+++ src/main/java/com/raincloud/sunlightmarket/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.raincloud.sunlightmarket.user.repository;
+
+import com.raincloud.sunlightmarket.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
