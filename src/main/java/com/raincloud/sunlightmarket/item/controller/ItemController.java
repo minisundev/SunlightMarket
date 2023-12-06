234a8af591a0a45f577dc490bdace5ac5fbe30df
Minsun <minisundev@gmail.com>
2023-12-06 13:40:56 +0900

    Chore: 주석과 path 수정

--- src/main/java/com/raincloud/sunlightmarket/item/controller/ItemController.java
+++ src/main/java/com/raincloud/sunlightmarket/item/controller/ItemController.java
@@ -33,8 +33,8 @@ public class ItemController {
     }
 
 
-    //선택 상품 조회
-    @GetMapping("/get")
+    //전체 상품 조회
+    @GetMapping("")
     public ResponseEntity<ItemAllResponseDto> getItems(
             @RequestParam String type
     ) {
