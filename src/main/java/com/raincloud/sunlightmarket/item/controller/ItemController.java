0432d4045da728e22ca6fec8a09e7f14787cada9
Minsun <minisundev@gmail.com>
2023-12-06 19:25:20 +0900

    Refactor: CommonResponse상속 대신 제너릭으로 개선

--- src/main/java/com/raincloud/sunlightmarket/item/controller/ItemController.java
+++ src/main/java/com/raincloud/sunlightmarket/item/controller/ItemController.java
@@ -1,5 +1,7 @@
 package com.raincloud.sunlightmarket.item.controller;
 
+import com.raincloud.sunlightmarket.global.dto.ApiResponse;
+import com.raincloud.sunlightmarket.global.security.UserDetailsImpl;
 import com.raincloud.sunlightmarket.item.dto.ItemAllResponseDto;
 import com.raincloud.sunlightmarket.item.dto.ItemRequestDto;
 import com.raincloud.sunlightmarket.item.dto.ItemResponseDto;
@@ -8,6 +10,7 @@ import com.raincloud.sunlightmarket.item.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.RejectedExecutionException;
@@ -20,17 +23,17 @@ public class ItemController {
     private final ItemService itemService;
 
     //상품 등록
+    //상품 등록
     @PostMapping("/add")
-    public ResponseEntity<ItemResponseDto> addItem(
+    public ApiResponse<ItemResponseDto> addItem(
             @RequestBody ItemRequestDto requestDto
-
     ) {
         try {
             ItemResponseDto responseDto = itemService.addItem(requestDto);
-            return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
-        } catch (RejectedExecutionException | IllegalArgumentException ex) {
-            return ResponseEntity.badRequest().body(new ItemResponseDto(ex.getMessage(), HttpStatus.BAD_REQUEST.value()));
-
+            ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+            return new ApiResponse<ItemResponseDto>(HttpStatus.CREATED.value(),"아이템 추가 성공했습니다",responseDto);
+        }catch (RejectedExecutionException | IllegalArgumentException ex){
+            return new ApiResponse<ItemResponseDto>(HttpStatus.BAD_REQUEST.value(),ex.getMessage());
         }
     }
 
@@ -52,36 +55,35 @@ public class ItemController {
 
     //선택 상품 조회
     @GetMapping("/{itemId}")
-    public ResponseEntity<ItemResponseDto> getItem(
+    public ApiResponse<ItemResponseDto> getItem(
             @PathVariable Long itemId
     ) {
         try {
             ItemResponseDto responseDto = itemService.getItem(itemId);
-            return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+            return new ApiResponse<ItemResponseDto>(HttpStatus.OK.value(),"아이템 조회에 성공했습니다",responseDto);
         }catch (RejectedExecutionException | IllegalArgumentException ex){
-            return ResponseEntity.badRequest().body(new ItemResponseDto(ex.getMessage(), HttpStatus.BAD_REQUEST.value()));
-
+            return new ApiResponse<ItemResponseDto>(HttpStatus.BAD_REQUEST.value(),ex.getMessage());
         }
     }
 
     //전체 상품 조회
     @GetMapping("")
-    public ResponseEntity<ItemAllResponseDto> getItems(
+    public ApiResponse<ItemAllResponseDto> getItems(
             @RequestParam String type
     ) {
         if(type.equals("All")){ return getAllItems();}
-        else if(type.equals("Myselect")){return ResponseEntity.badRequest().body(new ItemAllResponseDto("myselect", HttpStatus.BAD_REQUEST.value()));}
-        else{return ResponseEntity.badRequest().body(new ItemAllResponseDto("올바르지 않은 요청입니다", HttpStatus.BAD_REQUEST.value()));}
+        else if(type.equals("Myselect")){return new ApiResponse<ItemAllResponseDto>(HttpStatus.BAD_REQUEST.value(),"myselect");}
+        else{return new ApiResponse<ItemAllResponseDto>(HttpStatus.BAD_REQUEST.value(),"올바르지 않은 요청입니다");}
     }
 
-    public ResponseEntity<ItemAllResponseDto> getAllItems()
+    public ApiResponse<ItemAllResponseDto> getAllItems()
     {
         try {
             ItemAllResponseDto responseDto = new ItemAllResponseDto();
             responseDto.setItemResponseDtos(itemService.getAllItems());
-            return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+            return new ApiResponse<ItemAllResponseDto>(HttpStatus.OK.value(),"아이템 조회에 성공했습니다",responseDto);
         }catch (RejectedExecutionException | IllegalArgumentException ex){
-            return ResponseEntity.badRequest().body(new ItemAllResponseDto(ex.getMessage(), HttpStatus.BAD_REQUEST.value()));
+            return new ApiResponse<ItemAllResponseDto>(HttpStatus.BAD_REQUEST.value(),ex.getMessage());
         }
     }
 
