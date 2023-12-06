fee5cda4729651cb2bc2429bd4a72360b14f6436
doyoon <doyoun0527@naver.com>
2023-12-06 15:13:08 +0900

    Feat: itemId로 Item Update 기능 구현

--- src/main/java/com/raincloud/sunlightmarket/item/controller/ItemController.java
+++ src/main/java/com/raincloud/sunlightmarket/item/controller/ItemController.java
@@ -2,14 +2,12 @@ package com.raincloud.sunlightmarket.item.controller;
 
 import com.raincloud.sunlightmarket.item.dto.ItemRequestDto;
 import com.raincloud.sunlightmarket.item.dto.ItemResponseDto;
+import com.raincloud.sunlightmarket.item.dto.ItemUpdateRequest;
 import com.raincloud.sunlightmarket.item.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.RejectedExecutionException;
 
@@ -22,14 +20,27 @@ public class ItemController {
 
     @PostMapping("/post")
     public ResponseEntity<ItemResponseDto> addPost(
-            @RequestBody ItemRequestDto requestDto
+        @RequestBody ItemRequestDto requestDto
     ) {
         try {
             ItemResponseDto responseDto = itemService.addItem(requestDto);
             return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
-        }catch (RejectedExecutionException | IllegalArgumentException ex){
+        } catch (RejectedExecutionException | IllegalArgumentException ex) {
             return ResponseEntity.badRequest().body(new ItemResponseDto(ex.getMessage(), HttpStatus.BAD_REQUEST.value()));
 
         }
     }
+
+    @PutMapping("/items/{itemsId)")
+    public ResponseEntity<Void> updatePost(
+        @PathVariable Long itemId,
+        @RequestBody ItemUpdateRequest request
+    ) {
+        itemService.updateItem(itemId, request.getTitle());
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
+
+
 }
--- src/main/java/com/raincloud/sunlightmarket/item/service/ItemService.java
+++ src/main/java/com/raincloud/sunlightmarket/item/service/ItemService.java
@@ -2,20 +2,32 @@ package com.raincloud.sunlightmarket.item.service;
 
 import com.raincloud.sunlightmarket.item.dto.ItemRequestDto;
 import com.raincloud.sunlightmarket.item.dto.ItemResponseDto;
+import com.raincloud.sunlightmarket.item.dto.ItemUpdateRequest;
 import com.raincloud.sunlightmarket.item.entity.Item;
 import com.raincloud.sunlightmarket.item.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
 
     private final ItemRepository itemRepository;
+
     public ItemResponseDto addItem(ItemRequestDto requestDto) {
         // Dto -> Entity
         Item item = new Item(requestDto);
         Item saveItem = itemRepository.save(item);
         return new ItemResponseDto(item);
     }
+
+
+    @Transactional
+    public void updateItem(Long itemId, String title) {
+        Item findItem = itemRepository.findById(itemId).orElseThrow(NullPointerException::new);
+        findItem.updateTitle(title);
+    }
 }
+
+
