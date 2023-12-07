2fe75c1a90525c4c60a547db521c5d8dba2a7a14
tast <ioae7979@gmail.com>
2023-12-07 16:37:44 +0900

    feat: Review To Seller 리뷰 수정,삭제 기능 구현

--- src/main/java/com/raincloud/sunlightmarket/review/toseller/controller/ReviewToSellerController.java
+++ src/main/java/com/raincloud/sunlightmarket/review/toseller/controller/ReviewToSellerController.java
@@ -3,13 +3,16 @@ package com.raincloud.sunlightmarket.review.toseller.controller;
 import com.raincloud.sunlightmarket.global.security.UserDetailsImpl;
 import com.raincloud.sunlightmarket.review.toseller.dto.request.ReviewToSellerRequestDto;
 import com.raincloud.sunlightmarket.review.toseller.dto.response.CreateReviewToSellerResponseDto;
+import com.raincloud.sunlightmarket.review.toseller.dto.response.UpdateReviewToSellerResponseDto;
 import com.raincloud.sunlightmarket.review.toseller.service.ReviewToSellerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,10 +28,34 @@ public class ReviewToSellerController {
     public ResponseEntity<CreateReviewToSellerResponseDto> createReview(
         @PathVariable Long sellerId,
         @AuthenticationPrincipal UserDetailsImpl userDetails,
-        @RequestBody ReviewToSellerRequestDto reqeustDto) {
+        @RequestBody ReviewToSellerRequestDto requestDto) {
 
         CreateReviewToSellerResponseDto responseDto =
-            reviewToSellerService.createReview(sellerId, userDetails.getUser(), reqeustDto);
+            reviewToSellerService.createReview(sellerId, userDetails.getUser(), requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
+
+    @PutMapping("/{reviewId}")
+    public ResponseEntity<UpdateReviewToSellerResponseDto> updateReview(
+        @PathVariable Long sellerId,
+        @PathVariable Long reviewId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails,
+        @RequestBody ReviewToSellerRequestDto requestDto) {
+
+        UpdateReviewToSellerResponseDto responseDto =
+            reviewToSellerService.updateReview(sellerId, reviewId, userDetails.getUser(),
+                requestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+    }
+
+    @DeleteMapping("/{reviewId}")
+    public ResponseEntity<String> deleteReview(
+        @PathVariable Long sellerId,
+        @PathVariable Long reviewId,
+        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+
+        reviewToSellerService.deleteReview(sellerId,reviewId,userDetails.getUser());
+        return ResponseEntity.status(HttpStatus.OK).body("해당 리뷰가 삭제 되었습니다.");
+    }
+
 }
---
+++ src/main/java/com/raincloud/sunlightmarket/review/toseller/dto/response/UpdateReviewToSellerResponseDto.java
@@ -0,0 +1,25 @@
+package com.raincloud.sunlightmarket.review.toseller.dto.response;
+
+import com.raincloud.sunlightmarket.review.toseller.entity.ReviewToSeller;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+public class UpdateReviewToSellerResponseDto {
+
+    private Long id;
+    private String nickname;
+    private String comment;
+    private Boolean liked;
+    private LocalDateTime date;
+
+    public UpdateReviewToSellerResponseDto(ReviewToSeller review) {
+        this.id = review.getId();
+        this.nickname = review.getUser().getNickname();
+        this.comment = review.getComment();
+        this.liked = review.getLiked();
+        this.date = review.getModifiedAt();
+    }
+
+
+}
