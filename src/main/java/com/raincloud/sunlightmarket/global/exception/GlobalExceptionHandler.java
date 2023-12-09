4384f633a67188638d20eb5f3c1099e5cc8e6b2e
Minsun <minisundev@gmail.com>
2023-12-10 00:03:27 +0900

    Feat : EntityNotFoundException 에러 핸들러 추가

--- src/main/java/com/raincloud/sunlightmarket/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/raincloud/sunlightmarket/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.raincloud.sunlightmarket.global.exception;
 
 import com.raincloud.sunlightmarket.global.dto.ApiResponse;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,6 +22,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({RejectedExecutionException.class})
     public ApiResponse<?> handleRejectedExecutionException(RejectedExecutionException ex){
+        return new ApiResponse<>(HttpStatus.BAD_REQUEST.value(),ex.getMessage());
+    }
+
+    @ExceptionHandler({EntityNotFoundException.class})
+    public ApiResponse<?> handleEntityNotFoundException(EntityNotFoundException ex){
         return new ApiResponse<>(HttpStatus.BAD_REQUEST.value(),ex.getMessage());
     }
 
